80a55a551ee74ce0e51698c016e98ea76706ec42
Matthew Wong <matthewwong3.14@gmail.com>
2018-03-17 00:59:18 -0700

    menu scene and menu navigation implemented

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject menuCanvas;
+    public GameObject instrCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void activateInstruction()
+    {
+        instrCanvas.SetActive(true);
+        menuCanvas.SetActive(false);
+    }
+
+    public void activateMenu()
+    {
+        menuCanvas.SetActive(true);
+        instrCanvas.SetActive(false);
+    }
+}
