80e8591997e1165c352356e11e949053c57c1da6
Matthew Wong <matthewwong3.14@gmail.com>
2018-04-13 02:20:12 -0700

    Health, AI healthbar UI, Saving scores, and loading scores.

---
+++ Assets/Scripts/AIHealth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIHealth : MonoBehaviour {
+
+    public GameObject healthBar;
+    public float max_health = 100f;
+    public float cur_health = 0f;
+
+	// Use this for initialization
+	void Start () {
+        cur_health = max_health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //taking damage test
+        /*if (Input.GetKeyDown("f"))
+        {
+            takeDamage(11.0f);
+        }*/
+	}
+
+    //Take damage and check if still alive
+    void takeDamage(float damage)
+    {
+        cur_health -= damage;
+        updateHealthBar(cur_health / max_health);
+        if(cur_health <= 0)
+        {
+            die();
+        }
+    }
+
+    //When health reaches 0, destroy this gameObject
+    void die()
+    {
+        Destroy(gameObject);
+    }
+
+    //Update the health bar
+    void updateHealthBar(float healthPercent)
+    {
+        healthBar.transform.localScale = new Vector3(healthPercent, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
+}
--- Assets/Scripts/Scenemanager.cs
+++ Assets/Scripts/Scenemanager.cs
@@ -1,13 +1,72 @@
 ﻿using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class PlayerData
+{
+    int[] hiScores;
+
+    public PlayerData(int numHighScores)
+    {
+        hiScores = new int[numHighScores];
+    }
+
+    public void SetScore(int i, int score)
+    {
+        hiScores[i] = score;
+    }
+
+    public int GetScore(int i)
+    {
+        return hiScores[i];
+    }
+
+    public void Save(string path)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(path);
+
+        bf.Serialize(file, this);
+        file.Close();
+    }
+
+    public static PlayerData Load(string path)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(path, FileMode.Open);
+
+        PlayerData data = (PlayerData)bf.Deserialize(file);
+        file.Close();
+        return data;
+    }
+}
 
 public class Scenemanager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        //Testing saving and loading highscores
+        /*
+        PlayerData pData = new PlayerData(10);
+
+        for(int i = 0; i < 10; i++)
+        {
+            pData.SetScore(i, i * 10);
+        }
+
+        pData.Save(Application.persistentDataPath + "/HighScores.dat");
+
+        PlayerData pData2 = PlayerData.Load(Application.persistentDataPath + "/HighScores.dat");
+
+        for(int i = 0; i < 10; i++)
+        {
+            Debug.Log(pData2.GetScore(i));
+        }
+        */
 	}
 	
 	// Update is called once per frame
